3e619d4d6b12c748d561e78882eadd8915c647f0
ickw <ichi.dd@gmail.com>
2017-10-26 14:33:54 +0900

    finised ep1

---
+++ gameofchats/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by daiki ichikawa on 2017/10/26.
+//  Copyright © 2017年 Daiki Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+    }
+
+    func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+        
+    }
+
+}
+
